9985a7444e7043d788dddec1baf09979048debf4
satot <ph1.ph2.sa25@gmail.com>
2017-07-29 18:58:24 +0800

    Added error handling for unexisting resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::RoutingError, with: :render_404
+
+  def render_404
+    render template: 'errors/error_404', status: 404, layout: 'application',
+      content_type: 'application/json'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :suppliers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path', controller: 'application', action: 'render_404'
 end
